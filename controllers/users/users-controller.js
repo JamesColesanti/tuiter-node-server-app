43a2a4b96a35c035e76430dc0bf7e7f77627753e
James Colesanti <jamescolesanti@.yahoo.com>
2023-03-27 16:09:17 -0400

    working on lab

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -2,7 +2,8 @@ import people from './users.js'
 let users = people
 
 const UserController = (app) => {
-    app.get('/api/users', findUsers)
+    app.get('/api/users', findUsers);
+    app.get('/api/users/:uid', findUserById);
 }
 const findUsers = (req, res) => {
     const type = req.query.type
@@ -13,6 +14,13 @@ const findUsers = (req, res) => {
         return
     }
     res.json(users)
+}
+
+const findUserById = (req, res) => {
+    const userId = req.params.uid;
+    const user = users
+    .find(u => u._id === userId);
+    res.json(user);
 }
 
 export default UserController
