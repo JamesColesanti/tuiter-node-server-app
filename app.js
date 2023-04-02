05b5cc2a00714bacbe52c0f9c62a04764134e6b5
James Colesanti <jamescolesanti@.yahoo.com>
2023-04-02 11:47:03 -0400

    Finished rough draft

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 
-const app = express()
-HelloController(app)
-UserController(app)
-app.listen(4000)
+const app = express();
+app.use(cors());
+app.use(express.json());
+HelloController(app);
+UserController(app);
+TuitsController(app);
+app.listen(process.env.PORT || 4000);
